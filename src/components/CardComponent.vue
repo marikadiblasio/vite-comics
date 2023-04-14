d69c140036990ead87aaabb2e18d0595aaf0bb81
marikadiblasio <marbla.albram@gmail.com>
2023-04-14 17:03:45 +0200

    add CardComponent

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,15 +1,22 @@
 <template>
-    <div>
-        
+    <div class="col-12 col-md-4 col-lg-2">
+            <img :src="image" :alt="title">
+            <div class="text-uppercase small py-2">
+                {{ title }}
+            </div>
     </div>
 </template>
 
 <script>
     export default {
-        
+        name: 'CardComponent',
+        props: ['image', 'title']
     }
 </script>
 
 <style lang="scss" scoped>
-
+    img{
+        width: 10rem;
+        height: 10rem;
+    }
 </style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,25 +1,32 @@
 <template>
-    <div class="wrapper">
-        <div class="hero">
+    <div class="wrapper ">
+        <div class="hero position-relative">
             <img src="../assets/img/jumbotron.jpg" alt="hero">
+            <button class="text-uppercase fw-bold fs-5 btn btn-primary rounded-0">Current Series</button>
         </div>
-        <div class="bg-black h-100">
-            <div class="container text-white">
-                <button class="text-uppercase fw-bold fs-5 btn btn-primary rounded-0">Current Series</button>
+        <div class="bg-black">
+            <div class="container  h-100 text-white">
                 <div class="row">
-                    <CardComponent />
+                    <CardComponent :image="card.thumb" :title="card.series" v-for="card in cards"/>
                 </div>
+                <button class="text-uppercase mx-auto d-block btn btn-primary rounded-0">Load More</button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import {cards} from '../data/data';
     import CardComponent from './CardComponent.vue';
     export default {
         name: 'MainComponent',
         components: {
             CardComponent
+        },
+        data(){
+            return{
+                cards: [...cards]
+            }
         }
     }
 </script>
@@ -30,16 +37,24 @@
     }
     .hero {
         width: 100%;
-        height: 350px;
+        height: 300px;
         img {
             display: block;
             width: 100%;
-            height: 350px;
+            height: 300px;
             object-fit: cover;
             object-position: top;
         }
     }
     button.fs-5{
-        transform: translate(0, -50%);
+        // transform: translate(0, -50%);
+        position: absolute;
+        bottom: -5%;
+        left: 3%;
+        z-index: 100;
+    }
+    .bg-black{
+        height: calc(100% - 300px);
+        overflow-y: auto;
     }
 </style>
